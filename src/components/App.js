35025dcaa77d19b86c1fb3dfbd2addd3601e230f
Christopher Kramer <Hendrix1957@gmail.com>
2017-03-08 17:07:30 -0500

    starting

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,55 @@ import React, { Component } from 'react'
 // import videoURL from 'video/swatch.mp4'
 class App extends Component {
 
+  constructor () {
+    super()
+    this.state = ({
+      isNightTime: false,
+      visibleCategory: null
+    })
+  }
+
+  showCategory (category) {
+    this.setState({
+      visibleCategory: category
+    })
+  }
+
+  hideCategory (category) {
+    this.setState({
+      visibleCategory: null
+    })
+  }
+
+  isNightTime (event) {
+    event.preventDefault()
+    this.setState = ({
+    })
+  }
+
   render () {
-    return <div>
-      <nav id='nav'>
-        <ul id='navbar'>
-          <li id='homeicons'>1</li>
-          <li id='homeicons'>2</li>
-          <li id='homeicons'>3</li>
-        </ul>
-      </nav>
-      <h2>Kramer's
+    return <div id='page-wrap'>
+      <input type='checkbox' id='sidebartoggler' name='' value='' />
+      <div id='page-content'>
+        <label htmlFor='sidebartoggler' id='toggle'>☰</label>
+        <nav id='sidebar'>
+          <a href='' id='toggle'>☰</a>
+          <ul id='sidebar'>
+            <li id='homeicons'>Home</li>
+            <li id='homeicons'>Mens</li>
+            <li id='homeicons'>Sale</li>
+          </ul>
+        </nav>
+        <h2>Kramer's
         <br />
         Department
         <br />
         Store
       </h2>
-      {/* <video autoPlay loop muted id='fullscreen'>
-        <source src={videoURL} type='video/mp4' />
-      </video> */}
+        <button onClick={this.state.isNightTime}>
+        Night Mode
+      </button>
+      </div>
     </div>
   }
 }

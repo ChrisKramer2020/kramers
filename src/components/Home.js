4f2ace5a57da7790d6647c98fe8efb45997e5ad1
Christopher Kramer <Hendrix1957@gmail.com>
2017-04-22 17:14:33 -0400

    user data

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,3 +16,51 @@ class Home extends Component {
 }
 
 export default Home
+
+// var user = newUser('Mahesha')
+user.addSession('2017-02-05', 120)
+user.addSession('2017-02-06', 35)
+user.getData().total
+
+var User = function (name) {
+  var sessions = []
+  var totalDuration = 0
+
+  this.addSession = function (sessionDate, duration) {
+    sessions.push({
+      'sessionDate': sessionDate,
+      'duration': duration
+    })
+
+    totalDuration += duration
+
+    return totalDuration
+  }
+  this.getData = function () {
+    return {
+      'name': name,
+      'total': totalDuration,
+      'sessions': sessions.slice()
+    }
+  }
+}
+
+var buildUser = function (userData) {
+  var user = new User(userData.name)
+  userData.sessions.forEach(function (sesh) {
+    user.addSession(sesh.sessionDate, sesh.duration)
+  })
+  return user
+}
+
+var userData = {
+  'name': 'Mahesha',
+  'sessions': [
+    {'sessionDate': '2017-02-05', 'duration': 120},
+    {'sessionDate': '2017-02-06', 'duration': 35},
+    {'sessionDate': '2017-02-06', 'duration': 45}
+  ]
+}
+
+var user = buildUser(userData)
+console.log(user.addSession('2017-02-15', 40))
